83750427e9da1c9749a888e13073d562c845c3bc
Lucas Gabriel <102253585+LucasLgpp-06@users.noreply.github.com>
2023-04-10 08:37:24 -0300

    comentario teste autoload

--- App/autoload.php
+++ App/autoload.php
@@ -2,6 +2,7 @@
 
 spl_autoload_register(function($nome_da_classe)
 {
+    //comentario teste
     $arquivo = BASEDIR . '/' . $nome_da_classe . '.php';
 
     if (file_exists($arquivo)) include $arquivo;
