5a5af86896266307595f3d2a6f999817db681f3b
Lucas Gabriel <pintolucas996@gmail.com>
2023-04-24 09:03:44 -0300

    Tela inicial e CadVeiculos

--- App/Views/Cadastro/CadVeiculo.php
+++ App/Views/Cadastro/CadVeiculo.php
@@ -20,105 +20,158 @@
         {
             height: auto;    
         }
+        #texto_esquerda
+        {
+            position:relative;
+            right:-300px;
+            width:123px;
+            top:20px;
+            height:520px;
+        }
+        #inputs_direita
+        {
+            position:relative;
+            left:500px;
+            bottom:600px;
+            width:200px;
+        }
+        #cad_veiculos
+        {
+            width: 1000px;
+            background-color: rgba(0, 0, 0, 0.9);
+            border-radius: 15px;
+            color: #fff;
+        }
+        #btnEnv_cadVeiculo
+        {
+            position:relative;
+            bottom:200px;
+        }
+        select
+        {
+            width: 225px;
+        }
+
     </style>
 
 </head>
 
 <body>
-    <div>
+    <div id="cad_veiculos">
     <form method="post" action="/veiculo/form/save">
-        <h1>Veículo</h1>
+        <h1 style="text-align:center;">Veículo</h1>
 
         <input type="hidden" value="<?= $model_veiculo->id ?>" name="id" />
 
-        <label for="modelo_veiculo">Modelo:</label>
-        <input type="text" name="modelo_veiculo" id="modelo_veiculo" value="<?= $model_veiculo->modelo ?>"
-                maxlength="255"><br>
+        <div id="texto_esquerda">
+            <label for="id_marca_veiculo">Marca:</label><br>
+            <label for="id_fabricante_veiculo">Fabricante:</label><br>
+            <label for="id_tipo_veiculo">Tipo de Veículo:</label><br>
+            <label for="id_combustivel_veiculo">Combustível:</label><br><br>
+            <label for="modelo_veiculo">Modelo:</label><br><br><br>
+            <label for="ano_veiculo">Ano:</label><br><br><br>
+            <label for="cor_veiculo">Cor:</label><br><br>
+            <label for="numero_chassi_veiculo">Número do Chassi:</label><br><br><br>
+            <label for="km_veiculo">KM:</label><br><br><br>
+            <label for="revisao_veiculo">Revisão:</label><br>
+            <label for="sinistro_veiculo">Sinistro:</label><br>
+            <label for="roubo_furto_veiculo">Roubo ou Furto:</label><br>
+            <label for="aluguel_veiculo">Aluguel:</label><br>
+            <label for="venda_veiculo">Venda:</label><br>
+            <label for="particular_veiculo">Particular:</label><br><br>
+            <label for="observacao_veiculo">Observação:</label><br>
+        </div>
 
-        <label for="ano_veiculo">Ano:</label>
-        <input type="year" name="ano_veiculo" id="ano_veiculo" value="<?= $model_veiculo->ano ?>"
-                maxlength="4" minlength="4" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');"><br>
 
-        <label for="cor_veiculo">Cor:</label>
-        <input type="text" name="cor_veiculo" id="cor_veiculo" value="<?= $model_veiculo->cor ?>"
-                maxlength="255"><br>
+        <div id="inputs_direita">
+            <select name="id_marca_veiculo" id="id_marca_veiculo">
+                <?php foreach ($model_marca->rows as $item_marca): ?>
+                    <?php if (count($model_marca->rows) == 0):?>
+                        <span>Nenhum registro encontrado.</span>
+                    <?php else: ?>
+                        <option value="<?= $item_marca->id ?>"><?= $item_marca->marca ?></option>
+                    <?php endif ?>
+                <?php endforeach ?>
+            </select><br>
 
-        <label for="numero_chassi_veiculo">Número do Chassi:</label>
-        <input type="text" name="numero_chassi_veiculo" id="numero_chassi_veiculo" value="<?= $model_veiculo->numero_chassi ?>"
-                maxlength="17" minlength="17"><br>
+            
+            <select name="id_fabricante_veiculo" id="id_fabricante_veiculo">
+                <?php foreach ($model_fabricante->rows as $item_fabricante): ?>
+                    <?php if (count($model_fabricante->rows) == 0):?>
+                        <span>Nenhum registro encontrado.</span>
+                    <?php else: ?>
+                        <option value="<?= $item_fabricante->id ?>"><?= $item_fabricante->nome ?></option>
+                    <?php endif ?>
+                <?php endforeach ?>
+            </select><br>
 
-        <label for="km_veiculo">KM:</label>
-        <input type="text" name="km_veiculo" id="km_veiculo" value="<?= $model_veiculo->km ?>"
-                maxlength="255" minlength="1"><br>
+            
+            <select name="id_tipo_veiculo" id="id_tipo_veiculo">
+                <?php foreach ($model_tipo->rows as $item_tipo): ?>
+                    <?php if (count($model_tipo->rows) == 0):?>
+                        <span>Nenhum registro encontrado.</span>
+                    <?php else: ?>
+                        <option value="<?= $item_tipo->id ?>"><?= $item_tipo->tipo ?></option>
+                    <?php endif ?>
+                <?php endforeach ?>
+            </select><br>
 
-        <label for="revisao_veiculo">Revisão:</label>
-        <input type="checkbox" name="revisao_veiculo" id="revisao_veiculo"><br>
+            
+            <select name="id_combustivel_veiculo" id="id_combustivel_veiculo">
+                <?php foreach ($model_combustivel->rows as $item_combustivel): ?>
+                    <?php if (count($model_combustivel->rows) == 0):?>
+                        <span>Nenhum registro encontrado.</span>
+                    <?php else: ?>
+                        <option value="<?= $item_combustivel->id ?>"><?= $item_combustivel->nome ?></option>
+                    <?php endif ?>
+                <?php endforeach ?>
+            </select><br>
 
-        <label for="sinistro_veiculo">Sinistro:</label>
-        <input type="checkbox" name="sinistro_veiculo" id="sinistro_veiculo"><br>
+            
+            
+            <input type="text" name="modelo_veiculo" id="modelo_veiculo" value="<?= $model_veiculo->modelo ?>"
+                    maxlength="255"><br>
 
-        <label for="roubo_furto_veiculo">Roubo ou Furto:</label>
-        <input type="checkbox" name="roubo_furto_veiculo" id="roubo_furto_veiculo"><br>
+            
+            <input type="text" name="ano_veiculo" id="ano_veiculo" value="<?= $model_veiculo->ano ?>"
+                    maxlength="4" minlength="4"><br>
 
-        <label for="aluguel_veiculo">Aluguel:</label>
-        <input type="checkbox" name="aluguel_veiculo" id="aluguel_veiculo"><br>
+            
+            <input type="text" name="cor_veiculo" id="cor_veiculo" value="<?= $model_veiculo->cor ?>"
+                    maxlength="255"><br>
 
-        <label for="venda_veiculo">Venda:</label>
-        <input type="checkbox" name="venda_veiculo" id="venda_veiculo"><br>
+            
+            <input type="text" name="numero_chassi_veiculo" id="numero_chassi_veiculo" value="<?= $model_veiculo->numero_chassi ?>"
+                    maxlength="17" minlength="17"><br>
 
-        <label for="particular_veiculo">Particular:</label>
-        <input type="checkbox" name="particular_veiculo" id="particular_veiculo"><br>
+            
+            <input type="text" name="km_veiculo" id="km_veiculo" value="<?= $model_veiculo->km ?>"
+                    maxlength="255" minlength="1"><br><br>
 
-        <label for="observacao_veiculo">Observação:</label>
-        <input type="text" name="observacao_veiculo" id="observacao_veiculo" value="<?= $model_veiculo->observacao ?>"
-                maxlength="255"><br>
+            
+            <input type="checkbox" name="revisao_veiculo" id="revisao_veiculo"><br>
 
-        <label for="id_marca_veiculo">Marca:</label>
-        <select name="id_marca_veiculo" id="id_marca_veiculo">
-            <?php foreach ($model_marca->rows as $item_marca): ?>
-                <?php if (count($model_marca->rows) == 0):?>
-                    <span>Nenhum registro encontrado.</span>
-                <?php else: ?>
-                    <option value="<?= $item_marca->id ?>"><?= $item_marca->marca ?></option>
-                <?php endif ?>
-            <?php endforeach ?>
-        </select><br>
+            
+            <input type="checkbox" name="sinistro_veiculo" id="sinistro_veiculo"><br>
 
-        <label for="id_fabricante_veiculo">Fabricante:</label>
-        <select name="id_fabricante_veiculo" id="id_fabricante_veiculo">
-            <?php foreach ($model_fabricante->rows as $item_fabricante): ?>
-                <?php if (count($model_fabricante->rows) == 0):?>
-                    <span>Nenhum registro encontrado.</span>
-                <?php else: ?>
-                    <option value="<?= $item_fabricante->id ?>"><?= $item_fabricante->nome ?></option>
-                <?php endif ?>
-            <?php endforeach ?>
-        </select><br>
+            
+            <input type="checkbox" name="roubo_furto_veiculo" id="roubo_furto_veiculo"><br>
 
-        <label for="id_tipo_veiculo">Tipo de Veículo:</label>
-        <select name="id_tipo_veiculo" id="id_tipo_veiculo">
-            <?php foreach ($model_tipo->rows as $item_tipo): ?>
-                <?php if (count($model_tipo->rows) == 0):?>
-                    <span>Nenhum registro encontrado.</span>
-                <?php else: ?>
-                    <option value="<?= $item_tipo->id ?>"><?= $item_tipo->tipo ?></option>
-                <?php endif ?>
-            <?php endforeach ?>
-        </select><br>
+            
+            <input type="checkbox" name="aluguel_veiculo" id="aluguel_veiculo"><br>
 
-        <label for="id_combustivel_veiculo">Combustível:</label>
-        <select name="id_combustivel_veiculo" id="id_combustivel_veiculo">
-            <?php foreach ($model_combustivel->rows as $item_combustivel): ?>
-                <?php if (count($model_combustivel->rows) == 0):?>
-                    <span>Nenhum registro encontrado.</span>
-                <?php else: ?>
-                    <option value="<?= $item_combustivel->id ?>"><?= $item_combustivel->nome ?></option>
-                <?php endif ?>
-            <?php endforeach ?>
-        </select><br>
+            
+            <input type="checkbox" name="venda_veiculo" id="venda_veiculo"><br>
 
-        <br>
-        <button type="submit">Enviar</button>
+            
+            <input type="checkbox" name="particular_veiculo" id="particular_veiculo"><br>
+
+            
+            <input type="text" name="observacao_veiculo" id="observacao_veiculo" value="<?= $model_veiculo->observacao ?>"
+                    maxlength="255">
+        </div>
+
+        <button id="btnEnv_cadVeiculo" type="submit">Enviar</button>
 
     </form>
     
--- App/Views/Inicio.php
+++ App/Views/Inicio.php
@@ -14,21 +14,31 @@
         body
         {
             font-family: Arial, Helvetica, sans-serif;
-            background-image: linear-gradient(45deg, cyan, yellow);
             display: flex;
-            justify-content: center;
         }
-
-        #div1, #div2
+        #div1
         {
             background-color: rgba(0, 0, 0, 0.9);
             padding: 2.5%;
+            position:absolute;
+            left: -500px;
+            height:800px;
+            width:300px;
             border-radius: 15px;
             color: #fff;
-            margin: 1%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
+            z-index:2;
+        }
+        #div2{
+            z-index:2;
+            margin:10px;
+            background-color: rgba(0, 0, 0, 0.9);
+            padding: 2.5%;
+            position:absolute;
+            left:-200px;
+            height:800px;
+            width:300px;
+            border-radius: 15px;
+            color: #fff;
         }
 
         input
@@ -57,47 +67,75 @@
             background-color: deepskyblue;
             cursor: pointer;
         }
-        
+
+        img{
+            position:relative;
+            top:-1;
+            width:100%;
+            height:100%;
+        }
+
+        #aside {
+            z-index:1;
+            position:relative;
+            background: #000; 
+            width: 19%;
+            height: 490px; 
+            margin-top: 10px; 
+            margin-bottom: 10px;
+            margin-right:10px;
+        }
+
+
         
     </style>
 </head>
 
 <body>
-    
-    <div id="div1">
-        <div id="itemdiv">
-            <p>Combustível:</p>
-            <button onclick="document.location='/combustivel/form'" >Cadastro de Combustíveis</button><br>
-            <button onclick="document.location='/combustivel'" >Listagem de Combustíveis</button><br><br>
-        </div>
-        
-        <div id="itemdiv">
-            <p>Fabricante:</p>
-            <button onclick="document.location='/fabricante/form'" >Cadastro de Fabricantes</button><br>
-            <button onclick="document.location='/fabricante'" >Listagem de Fabricantes</button><br><br>
-        </div>
 
-        <div id="itemdiv">
-            <p>Marca:</p>
-            <button onclick="document.location='/marca/form'" >Cadastro de Marca</button><br>
-            <button onclick="document.location='/marca'" >Listagem de Marca</button><br><br>
-        </div>
-    </div>
+    <img src="Views/img/tela_fundo.jpg" height="969px" width="1990px" alt="carro na estrada">
     
-    <br>
-    <div id="div2">
-        <div id="itemdiv">
-            <p>Tipo:</p>
-            <button onclick="document.location='/tipo/form'" >Cadastro de Tipos de Veículos</button><br>
-            <button onclick="document.location='/tipo'" >Listagem de Tipos de Veículos</button><br><br>
-        </div>    
+ 
+    
+    <div id="aside">
+        <div id="div1">
+            <div class="itemdiv">
+                <h1>Cadastro de Veículos</h1>
+                <p>Combustível:</p>
+                <button onclick="document.location='/combustivel/form'" >Cadastro</button><br>
+                <button onclick="document.location='/combustivel'" >Listagem</button><br><br>
+            </div>
+            
+            <div class="itemdiv">
+                <p>Fabricante:</p>
+                <button onclick="document.location='/fabricante/form'" >Cadastro</button><br>
+                <button onclick="document.location='/fabricante'" >Listagem</button><br><br>
+            </div>
+
+
+            <div class="itemdiv">
+                <p>Marca:</p>
+                <button onclick="document.location='/marca/form'" >Cadastro</button><br>
+                <button onclick="document.location='/marca'" >Listagem</button><br><br>
+            </div>
+        </div>
         
-        <div id="itemdiv">
-            <p>Veículo:</p>
-            <button onclick="document.location='/veiculo/form'" >Cadastro de Veículos</button><br>
-            <button onclick="document.location='/veiculo'" >Listagem de Veículos</button><br><br>
+        
+        <div id="div2">
+            <div class="itemdiv">
+                <p>Tipo:</p>
+                <button onclick="document.location='/tipo/form'" >Cadastro</button><br>
+                <button onclick="document.location='/tipo'" >Listagem</button><br><br>
+            </div>    
+            
+            <div class="itemdiv">
+                <p>Veículo:</p>
+                <button onclick="document.location='/veiculo/form'" >Cadastro</button><br>
+                <button onclick="document.location='/veiculo'" >Listagem</button><br><br>
+            </div>
         </div>
     </div>
+
 
 
 
