1889de5703cb8b470127c1ca2fda74dc78811f60
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-07-01 15:26:37 -0300

    Arrumando página inicial.

--- App/Views/Inicio.php
+++ App/Views/Inicio.php
@@ -5,160 +5,173 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Início</title>
     <style>
+        *
+        {
+            margin: 0;
+            padding: 0;
+            border: 0;
+        }
+
         html
         {
             height: 100%;
-            width: 100%;
+            width: auto;
         }
-        
+
         body
         {
-            
+            width: 100%;
+            height: auto;
             font-family: Arial, Helvetica, sans-serif;
+            background-image: url("/Views/img/Fundo_Inicial.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
             display: flex;
             justify-content: center;
-            align-items: center;
-            align-content:center;
-            background-image: url('/Views/img/Fundo_Inicial.jpg');
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover; 
-        }
-        #div1
-        {
-            background-color: rgba(0, 0, 0, 0.9);
-            padding: 2.5%;
-            width:20%;
-            height:90%;
-            
-            color: #fff;
-            
-        }
-        #div2{
-            background-color: rgba(0, 0, 0, 0.9);
-            padding-top:15%;
-            padding: 2.5%;
-            height:580px;
-            width: 15%;
-            color: #fff;
-            align-items: center;
-            
-        }
-        #div2 .itemdiv {
-            position: relative;
-            bottom: 100px;
-        }
-        
-
-        input
-        {
-            padding: 15px;
-            outline: none;
-            font-size: 15px;
-            border-radius: 10px;
-
         }
 
         button
         {
-            text-align:right;
-            display:inline; 
+            text-align: center;
             background-color: dodgerblue;
             border: none;
-            padding: 15px;
-            width:30%;
-            border-radius:15px;
-            color: white;
-            font-size: 15px;
-            margin-top: 5px;
-            
+            border-radius: 12px;
+            color: #FFFFFF;
+            font-size: 12pt;
+            padding: 5px;
+            width: 100%;
+            min-width: 120px;
+            height: 100%;
+            min-height: 40px;
         }
 
-        button:hover{
+        button:hover
+        {
             background-color: deepskyblue;
-            cursor: pointer;
-            
+            cursor: pointer;    
         }
 
-        img{
-            position:relative;
-            top:-1;
-            width:100%;
-            height:100%;
+        #conteudo
+        {
+            width: 50%;
+            height: auto;
+            background-color: rgba(0, 0, 0, 0.9);
+            color: #FFFFFF;
+            margin: 1%;
+            padding: 2%;
+            border-radius: 15px;
         }
 
-        #aside {
-            position:relative;
-            background: #fff; 
-            width: 19%;
-            height: 490px; 
-            margin-top: 10px; 
-            margin-bottom: 10px;
-            margin-right:10px;
+        #conteudo h1
+        {
+            text-align: center;
         }
-        
 
+        #separacao
+        {
+            display: flex;
+            justify-content: space-around;
+            margin-top: 2%;
+            width: 100%;
+        }
 
-        
+        .item
+        {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+        }
+
+        .item h3
+        {
+            text-align: center;
+        }
+
+        .item-botao
+        {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+
+        .btn-espaco
+        {
+            margin-bottom: 5px;
+        }
     </style>
 </head>
 
 <body>
+    <div id="conteudo">
+        <h1>Cadastro de Veículos</h1>
+        <br><br>
 
-    <!-- <img src="Views/img/tela_fundo.jpg" height="969px" width="1990px" alt="carro na estrada"> -->
-    
- 
-    
-    <!-- <div id="aside"> -->
-        <div id="div1">
-            <div class="itemdiv">
-                <!-- <img src="C:\Users\Usuario\Desktop\CadastroVeiculos\App\Views\img\carro.png" alt=""> -->
-                <h1>Cadastro de Veículos</h1>
-                <p>Combustível:</p>
-                <button onclick="document.location='/combustivel/form'" >Cadastro</button><br>
-                <button onclick="document.location='/combustivel'" >Listagem</button><br><br>
+        <div id="separacao">
+            <div class="separacao-item">
+                <div class="item">
+                    <h3>Combustível:</h3>
+                    <br>
+                    <div class="item-botao">
+                        <button onclick="document.location='/combustivel/form'" class="btn-espaco">Cadastro</button>
+                        <button onclick="document.location='/combustivel'" >Listagem</button>                        
+                    </div>
+                </div>
+
+                <br><br>
+
+                <div class="item">
+                    <h3>Fabricante:</h3>
+                    <br>
+                    <div class="item-botao">
+                        <button onclick="document.location='/fabricante/form'" class="btn-espaco">Cadastro</button>
+                        <button onclick="document.location='/fabricante'" >Listagem</button>
+                    </div>
+                </div>
+
+                <br><br>
+
+                <div class="item">
+                    <h3>Marca:</h3>
+                    <br>
+                    <div class="item-botao">
+                        <button onclick="document.location='/marca/form'" class="btn-espaco">Cadastro</button>
+                        <button onclick="document.location='/marca'" >Listagem</button>
+                    </div>
+                </div>
             </div>
             
-            <div class="itemdiv">
-                <p>Fabricante:</p>
-                <button onclick="document.location='/fabricante/form'" >Cadastro</button><br>
-                <button onclick="document.location='/fabricante'" >Listagem</button><br><br>
-            </div>
+            <div class="separacao-item">
+                <div class="item">
+                    <h3>Tipo:</h3>
+                    <br>
+                    <button onclick="document.location='/tipo/form'" class="btn-espaco">Cadastro</button>
+                    <button onclick="document.location='/tipo'" >Listagem</button>
+                </div>
 
+                <br><br>
 
-            <div class="itemdiv">
-                <p>Marca:</p>
-                <button onclick="document.location='/marca/form'" >Cadastro</button><br>
-                <button onclick="document.location='/marca'" >Listagem</button><br><br>
-            </div>
-        </div>
-        
-        
-        <div id="div2">
-            <div class="itemdiv">
-                <br><br><br><br><br><br><br>
-                <p>Tipo:</p>
-                <button onclick="document.location='/tipo/form'" >Cadastro</button><br>
-                <button onclick="document.location='/tipo'" >Listagem</button><br><br>
-            </div>    
-            
-            <div class="itemdiv">
-                <p>Veículo:</p>
-                <button onclick="document.location='/veiculo/form'" >Cadastro</button><br>
-                <button onclick="document.location='/veiculo'" >Listagem</button><br><br>
-            </div>
+                <div class="item">
+                    <h3>Veículo:</h3>
+                    <br>
+                    <div class="item-botao">
+                        <button onclick="document.location='/veiculo/form'" class="btn-espaco">Cadastro</button>
+                        <button onclick="document.location='/veiculo'" >Listagem</button>
+                    </div>
+                </div>
 
-            <div class="itemdiv">
-                <p>Backup:</p>
-                <button onclick="document.location='/backup/exportar'">Exportar Banco</button><br><br>
-                <button onclick="document.location='/backup/importar'">Importar Banco</button><br><br>
+                <br><br>
+
+                <div class="item">
+                    <h3>Backup:</h3>
+                    <br>
+                    <div class="item-botao">
+                        <button onclick="document.location='/backup/exportar'" class="btn-espaco">Exportar</button>
+                        <button onclick="document.location='/backup/importar'" >Importar</button>
+                    </div>
+                </div>
             </div>
         </div>
-    <!--  </div> -->
-
-
-
-
+    </div>
 
 </body>
 </html>
